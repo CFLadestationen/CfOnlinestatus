baf8185a4cbb77110a392d0f498b81ec35e4401c
lordprize <lordprize@users.noreply.github.com>
2018-02-08 16:55:58 +0100

    Changed pin definition to a struct, Added name to S0 inputs
    Changed pin definition to a struct
    Added name to S0 inputs

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -4,70 +4,104 @@
  * Development thread (in German): https://www.goingelectric.de/forum/goingelectric-crowdfunding/neues-projekt-onlinestatus-fuer-crowdfunding-ladepunkte-t29325.html
  * 
  * Current features
- * - Read an arbitrary number of S0 inputs and print them on the serial console
- * - Serial format: s0_<number_starting_at_0>:<ms_between_last_2_impulses>:<impulses_since_last_output>:<seconds since last impulse>:<newline \n>
- * - Serial example:
- *     s0_0:300:6:0:  // s0 pin 0 had 300 ms between the last two impulses, 6 impulses since the last output and 0 seconds since the last impulse
- *     s0_1:465:0:20: // s0 pin 1 had 465 ms between the last two impulses, 0 impulses since the last output and 20 seconds since the last impulse
- * - Digital input pins can be configured with a custom name, output is di_<name>:<status on or off>:<newline \n>
+ * - Read an arbitrary number of S0 inputs and digital inputs
+ * - Print the status of those inputs on the serial console
+ * - Serial format for S0: s0_<pin_name>:<ms_between_last_2_impulses>:<impulses_since_last_output>:<seconds since last impulse>:<newline \n>
+ * - S0 example:
+ *     s0_CounterA:300:6:0:  // s0 pin CounterA had 300 ms between the last two impulses, 6 impulses since the last output and 0 seconds since the last impulse
+ *     s0_CounterB:465:0:20: // s0 pin CounterB had 465 ms between the last two impulses, 0 impulses since the last output and 20 seconds since the last impulse
+ * - Serial format for digital input: di_<pin_name>:<status on or off>:<newline \n>
  * - Digital input example:
  *     di_SpaceOccupied:on:
  *     di_ContactorOn:off:
- * 
- * Untested software - use at your own risk!
  */
-// ---- Begin of customizable section ----
+struct named_pin {
+  const char* pin_name;
+  const uint8_t pin_number;
+  const uint8_t on_value;
+  const uint8_t off_value;
+  const uint8_t pin_mode;
+};
 
-// Pin numbers of S0 input pins
-const uint8_t s0_pin[]  = {D1,    D2};
-// S0 pins high or low active? If low, switch both states
-const uint8_t s0_on[]   = {HIGH,  HIGH};
-const uint8_t s0_off[]  = {LOW,   LOW};
-// Set to INPUT_PULLUP instead of INPUT to activate the internal pull-up resistor
-const uint8_t s0_mode[] = {INPUT, INPUT};
-// Pin numbers and names of digital input pins
-const uint8_t digital_input_pin[]  = {D5,              D4};
-const char* digital_input_name[]   = {"SpaceOccupied", "ContactorOn"};
-// Digital input pins high or low active? Mode?
-const uint8_t digital_input_on[]   = {HIGH,            LOW};
-const uint8_t digital_input_off[]  = {LOW,             HIGH};
-const uint8_t digital_input_mode[] = {INPUT,           INPUT_PULLUP};
+// ---- Begin of customizable section ----
 // Send serial updates every ... ms
 const uint32_t output_delay = 5000;
+// Definition of S0 inputs
+const named_pin s0[] = {
+  {
+    "CounterA",  // pin name
+    D1,          // pin number
+    HIGH,        // "on" value
+    LOW,         // "off" value
+    INPUT        // pin mode
+  },
+  {
+    "CounterB",  // pin name
+    D2,          // pin number
+    LOW,         // "on" value
+    HIGH,        // "off" value
+    INPUT_PULLUP // pin mode    
+  }
+};
 
+// Definition of digital inputs
+const named_pin digital_input[] = {
+  {
+    "SpaceOccupied", // pin name
+    D5,              // pin number
+    HIGH,            // "on" value
+    LOW,             // "off" value
+    INPUT            // pin mode
+  },
+  {
+    "ContactorOn",   // pin name
+    D4,              // pin number
+    LOW,             // "on" value
+    HIGH,            // "off" value
+    INPUT_PULLUP     // pin mode    
+  }
+};
 // ---- End of customizable section ----
 
 // Internal variables
-uint32_t last_s0_millis[sizeof(s0_pin)];
-uint32_t last_s0_span[sizeof(s0_pin)];
-uint8_t  last_s0_state[sizeof(s0_pin)];
 uint32_t last_output;
-uint32_t impulses_since_output[sizeof(s0_pin)];
+const uint8_t s0_pincount = sizeof(s0)/sizeof(s0[0]);
+const uint8_t di_pincount = sizeof(digital_input)/sizeof(digital_input[0]);
+uint32_t last_s0_millis[s0_pincount];
+uint32_t last_s0_span[s0_pincount];
+uint8_t  last_s0_state[s0_pincount];
+uint32_t impulses_since_output[s0_pincount];
 
 void setup() {
   Serial.begin(115200);
   // wait for serial connection
   while(!Serial) {}
   
-  Serial.print("CfOnlinestatus starting with ");
-  Serial.print(sizeof(s0_pin));
-  Serial.print(" configured S0 pins and ");
-  Serial.print(sizeof(digital_input_pin));
-  Serial.println(" digital input pins.");
+  Serial.print("\n\nCfOnlinestatus starting with ");
+  Serial.print(s0_pincount);
+  Serial.print(" S0 pins and ");
+  Serial.print(di_pincount);
+  Serial.println(" digital input pins:");
   uint32_t current_time = millis();
   last_output = current_time;
   uint8_t val = 0;
-  for(uint8_t i = 0; i < sizeof(s0_pin); i++) {
-    pinMode(s0_pin[i], s0_mode[i]);
+  for(uint8_t i = 0; i < s0_pincount; i++) {
+    pinMode(s0[i].pin_number, s0[i].pin_mode);
     last_s0_millis[i] = current_time;
     last_s0_span[i] = 0;
-    last_s0_state[i] = s0_off[i];
+    last_s0_state[i] = s0[i].off_value;
     impulses_since_output[i] = 0;
+    Serial.print("Configured pin number ");
+    Serial.print(s0[i].pin_number);
+    Serial.print(" as S0 pin named ");
+    Serial.println(s0[i].pin_name);
   }
-  for(uint8_t i = 0; i < sizeof(digital_input_pin); i++) {
-    pinMode(digital_input_pin[i], digital_input_mode[i]);
-    Serial.print("Configured digital input pin ");
-    Serial.println(digital_input_name[i]);
+  for(uint8_t i = 0; i < di_pincount; i++) {
+    pinMode(digital_input[i].pin_number, digital_input[i].pin_mode);
+    Serial.print("Configured pin number ");
+    Serial.print(digital_input[i].pin_number);
+    Serial.print(" as digital input pin named ");
+    Serial.println(digital_input[i].pin_name);
   }
   Serial.println("CfOnlinestatus initialisation complete.");
 }
@@ -84,9 +118,9 @@ void loop() {
 
 void read_s0_inputs() {
   uint32_t current_time = millis();
-  for(uint8_t i = 0; i < sizeof(s0_pin); i++) {
-    uint8_t value = digitalRead(s0_pin[i]);
-    if(value == s0_on[i] && last_s0_state[i] == s0_off[i]) {
+  for(uint8_t i = 0; i < s0_pincount; i++) {
+    uint8_t value = digitalRead(s0[i].pin_number);
+    if(value == s0[i].on_value && last_s0_state[i] == s0[i].off_value) {
       // change detected
       last_s0_span[i] = (uint32_t)(current_time - last_s0_millis[i]); // cast to avoid rollover problems
       last_s0_millis[i] = current_time;
@@ -98,9 +132,9 @@ void read_s0_inputs() {
 
 void print_s0_status() {
   uint32_t current_time = millis();
-  for(uint8_t i = 0; i < sizeof(s0_pin); i++) {
+  for(uint8_t i = 0; i < s0_pincount; i++) {
     Serial.print("s0_");
-    Serial.print(i);
+    Serial.print(s0[i].pin_name);
     Serial.print(':');
     Serial.print(last_s0_span[i]);
     Serial.print(':');
@@ -113,11 +147,11 @@ void print_s0_status() {
 }
 
 void print_digital_input_status() {
-  for(uint8_t i = 0; i < sizeof(digital_input_pin); i++) {
+  for(uint8_t i = 0; i < di_pincount; i++) {
     Serial.print("di_");
-    Serial.print(digital_input_name[i]);
+    Serial.print(digital_input[i].pin_name);
     Serial.print(':');
-    if(digitalRead(digital_input_pin[i]) == digital_input_on[i]) {
+    if(digitalRead(digital_input[i].pin_number) == digital_input[i].on_value) {
       Serial.print("on");
     } else {
       Serial.print("off");
