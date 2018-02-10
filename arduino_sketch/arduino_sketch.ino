1b7e3b042342f69bac1b25ff08ad27841858b8f3
lordprize <lordprize@users.noreply.github.com>
2018-02-10 13:39:45 +0100

    Added #define for selection of features
    Many #define were added for the selection of hardware and software features. Sample output:
    
    CfOnlinestatus id CFMusterstadtGoethestr12 starting with following features:
    Hardware platform: WeMos D1 R2 & mini
    2 S0 pin(s)
    2 digital input pin(s)
    1 analog input pin(s)
    1 HC-SR04 ultrasound sensor(s)
    GSM connection (not implemented yet!)
    Configured pin number 5 as S0 pin named CounterA
    Configured pin number 4 as S0 pin named CounterB
    Configured pin number 14 as digital input pin named SpaceOccupied
    Configured pin number 2 as digital input pin named ContactorOn
    Configured pin number 17 as analog input pin named PPVoltage with low threshold 87 and high threshold 217
    Configured ultrasound sensor named CarDistance with echo pin 12, trigger pin 15 and timeout 20000 us
    CfOnlinestatus initialisation complete.
    id_CFMusterstadtGoethestr12:60068:
    s0_CounterA:41:1:59:
    s0_CounterB:0:0:60:
    di_SpaceOccupied:on:
    di_ContactorOn:off:
    ai_PPVoltage:0:low:
    us_CarDistance:0:0:

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -24,6 +24,25 @@
  *     us_CarDistance:5800:100: // 1m distance
  *     us_CarDistance:0:0:      // nothing in range (timeout occured)
  */
+// --------------------------------------------------------------
+// -------------- Begin of customizable section -----------------
+// --------------------------------------------------------------
+// Unique name for the charging station
+const char* chargepoint_id = "CFMusterstadtGoethestr12";
+// Send updates every ... ms
+const uint32_t output_delay = 60000;
+// Enable software features by uncommenting the #define directive
+#define INPUT_S0         1
+#define INPUT_DIGITAL    1
+#define INPUT_ANALOG     1
+#define INPUT_ULTRASOUND 1
+#define OUTPUT_SERIAL    1
+//#define OUTPUT_WIFI      1
+//#define OUTPUT_LAN       1
+//#define OUTPUT_LORA      1
+#define OUTPUT_GSM       1
+
+// Named pin data structure - don't change
 struct named_pin {
   const char* pin_name;
   const uint8_t pin_number;
@@ -31,19 +50,7 @@ struct named_pin {
   const uint8_t off_value;
   const uint8_t pin_mode;
 };
-struct ultrasound_sensor {
-  const char* sensor_name;
-  const uint8_t trigger_pin;
-  const uint8_t echo_pin;
-  const uint32_t timeout_us;
-  const uint8_t trigger_on;
-  const uint8_t trigger_off;
-  const uint8_t echo_on;
-};
-
-// ---- Begin of customizable section ----
-// Send serial updates every ... ms
-const uint32_t output_delay = 5000;
+#if defined(INPUT_S0)
 // Definition of S0 inputs
 const named_pin s0[] = {
   {
@@ -61,7 +68,9 @@ const named_pin s0[] = {
     INPUT_PULLUP // pin mode    
   }
 };
+#endif //INPUT_S0
 
+#if defined(INPUT_DIGITAL)
 // Definition of digital inputs
 const named_pin digital_input[] = {
   {
@@ -79,7 +88,9 @@ const named_pin digital_input[] = {
     INPUT_PULLUP     // pin mode    
   }
 };
+#endif //INPUT_DIGITAL
 
+#if defined(INPUT_ANALOG)
 // Definition of analog inputs
 // CAUTION: ESP8266 only has one analog input (A0) with a voltage range of 0V - 1V, 
 //          so you will need a 1:4 voltage divider (5V becomes 1V) to detect PP voltage.
@@ -93,7 +104,19 @@ const named_pin analog_input[] = {
     INPUT        // pin mode
   }
 };
+#endif //INPUT_ANALOG
 
+#if defined(INPUT_ULTRASOUND)
+// Ultrasound sensor data structure - don't change
+struct ultrasound_sensor {
+  const char* sensor_name;
+  const uint8_t trigger_pin;
+  const uint8_t echo_pin;
+  const uint32_t timeout_us;
+  const uint8_t trigger_on;
+  const uint8_t trigger_off;
+  const uint8_t echo_on;
+};
 // Definition of HC-SR04 ultrasound sensors
 const ultrasound_sensor us_sensor[] {
   {
@@ -106,55 +129,111 @@ const ultrasound_sensor us_sensor[] {
     HIGH           // Echo ON value
   }
 };
-
-// ---- End of customizable section ----
+#endif //INPUT_ULTRASOUND
+// --------------------------------------------------------------
+// -------------- End of customizable section -------------------
+// --------------------------------------------------------------
 
 // Internal variables
 uint32_t last_output;
+#if defined(INPUT_S0)
 const uint8_t s0_pincount = sizeof(s0)/sizeof(s0[0]);
-const uint8_t di_pincount = sizeof(digital_input)/sizeof(digital_input[0]);
-const uint8_t ai_pincount = sizeof(analog_input)/sizeof(analog_input[0]);
-const uint8_t us_pincount = sizeof(us_sensor)/sizeof(us_sensor[0]);
 uint32_t last_s0_millis[s0_pincount];
 uint32_t last_s0_span[s0_pincount];
 uint8_t  last_s0_state[s0_pincount];
 uint32_t impulses_since_output[s0_pincount];
+#endif //INPUT_S0
+#if defined(INPUT_DIGITAL)
+const uint8_t di_pincount = sizeof(digital_input)/sizeof(digital_input[0]);
+#endif //INPUT_DIGITAL
+#if defined(INPUT_ANALOG)
+const uint8_t ai_pincount = sizeof(analog_input)/sizeof(analog_input[0]);
+#endif //INPUT_ANALOG
+#if defined(INPUT_ULTRASOUND)
+const uint8_t us_pincount = sizeof(us_sensor)/sizeof(us_sensor[0]);
+#endif //INPUT_ULTRASOUND
 
 void setup() {
+#if defined(OUTPUT_SERIAL  )
   Serial.begin(115200);
   // wait for serial connection
   while(!Serial) {}
-  
-  Serial.print("\n\nCfOnlinestatus starting with ");
+#endif //OUTPUT_SERIAL
+
+#if defined(OUTPUT_SERIAL)
+  Serial.print("\n\nCfOnlinestatus id ");
+  Serial.print(chargepoint_id);
+  Serial.println(" starting with following features:");
+  Serial.print("Hardware platform: ");
+#if defined(ARDUINO_ESP8266_WEMOS_D1MINI)
+  Serial.println("WeMos D1 R2 & mini");
+#elif defined(ARDUINO_AVR_UNO)
+  Serial.println("Arduino/Genuino Uno");
+#else
+  Serial.println("unknown");
+#endif //Hardware
+#if defined(INPUT_S0)
   Serial.print(s0_pincount);
-  Serial.print(" S0 pins, ");
+  Serial.println(" S0 pin(s)");
+#endif //INPUT_S0
+#if defined(INPUT_DIGITAL)
   Serial.print(di_pincount);
-  Serial.print(" digital input pins and ");
+  Serial.println(" digital input pin(s)");
+#endif //INPUT_DIGITAL
+#if defined(INPUT_ANALOG)
   Serial.print(ai_pincount);
-  Serial.println(" analog input pins.");
+  Serial.println(" analog input pin(s)");
+#endif //INPUT_ANALOG
+#if defined(INPUT_ULTRASOUND)
+  Serial.print(us_pincount);
+  Serial.println(" HC-SR04 ultrasound sensor(s)");
+#endif //INPUT_ULTRASOUND
+#if defined(OUTPUT_WIFI)
+  Serial.println("WiFi connection (not implemented yet!)");
+#endif //OUTPUT_WIFI
+#if defined(OUTPUT_LAN)
+  Serial.println("LAN connection (not implemented yet!)");
+#endif //OUTPUT_LAN
+#if defined(OUTPUT_LORA)
+  Serial.println("LoRaWAN connection (not implemented yet!)");
+#endif //OUTPUT_LORA
+#if defined(OUTPUT_GSM)
+  Serial.println("GSM connection (not implemented yet!)");
+#endif //OUTPUT_GSM
+#endif //OUTPUT_SERIAL
+
   uint32_t current_time = millis();
   last_output = current_time;
-  uint8_t val = 0;
+#if defined(INPUT_S0)
   for(uint8_t i = 0; i < s0_pincount; i++) {
     pinMode(s0[i].pin_number, s0[i].pin_mode);
     last_s0_millis[i] = current_time;
     last_s0_span[i] = 0;
     last_s0_state[i] = s0[i].off_value;
     impulses_since_output[i] = 0;
+#if defined(OUTPUT_SERIAL)
     Serial.print("Configured pin number ");
     Serial.print(s0[i].pin_number);
     Serial.print(" as S0 pin named ");
     Serial.println(s0[i].pin_name);
+#endif //OUTPUT_SERIAL
   }
+#endif //INPUT_S0
+#if defined(INPUT_DIGITAL)
   for(uint8_t i = 0; i < di_pincount; i++) {
     pinMode(digital_input[i].pin_number, digital_input[i].pin_mode);
+#if defined(OUTPUT_SERIAL)
     Serial.print("Configured pin number ");
     Serial.print(digital_input[i].pin_number);
     Serial.print(" as digital input pin named ");
     Serial.println(digital_input[i].pin_name);
+#endif //OUTPUT_SERIAL
   }
+#endif //INPUT_DIGITAL
+#if defined(INPUT_ANALOG)
   for(uint8_t i = 0; i < ai_pincount; i++) {
     pinMode(analog_input[i].pin_number, analog_input[i].pin_mode);
+#if defined(OUTPUT_SERIAL)
     Serial.print("Configured pin number ");
     Serial.print(analog_input[i].pin_number);
     Serial.print(" as analog input pin named ");
@@ -163,11 +242,15 @@ void setup() {
     Serial.print(analog_input[i].off_value);
     Serial.print(" and high threshold ");
     Serial.println(analog_input[i].on_value);
+#endif //OUTPUT_SERIAL
   }
+#endif //INPUT_ANALOG
+#if defined(INPUT_ULTRASOUND)
   for(uint8_t i = 0; i < us_pincount; i++) {
     pinMode(us_sensor[i].echo_pin, INPUT);
     pinMode(us_sensor[i].trigger_pin, OUTPUT);
     digitalWrite(us_sensor[i].trigger_pin, us_sensor[i].trigger_off);
+#if defined(OUTPUT_SERIAL)
     Serial.print("Configured ultrasound sensor named ");
     Serial.print(us_sensor[i].sensor_name);
     Serial.print(" with echo pin ");
@@ -177,22 +260,44 @@ void setup() {
     Serial.print(" and timeout ");
     Serial.print(us_sensor[i].timeout_us);
     Serial.println(" us");
+#endif //OUTPUT_SERIAL
   }
+#endif //INPUT_ULTRASOUND
+#if defined(OUTPUT_SERIAL)
   Serial.println("CfOnlinestatus initialisation complete.");
+#endif //OUTPUT_SERIAL
 }
 
 void loop() {
   uint32_t current_time = millis();
+#if defined(INPUT_S0)
   read_s0_inputs();
+#endif //INPUT_S0
   if(((uint32_t)(current_time-last_output)) >= output_delay) {
     last_output = current_time;
-    print_s0_status(); 
+#if defined(OUTPUT_SERIAL)
+    Serial.print("id_");
+    Serial.print(chargepoint_id);
+    Serial.print(':');
+    Serial.print(current_time);
+    Serial.println(':');
+#endif //OUTPUT_SERIAL
+#if defined(INPUT_S0)
+    print_s0_status();
+#endif //INPUT_S0
+#if defined(INPUT_DIGITAL)
     print_digital_input_status();
+#endif //INPUT_DIGITAL
+#if defined(INPUT_ANALOG)
     print_analog_input_status();
+#endif //INPUT_ANALOG
+#if defined(INPUT_ULTRASOUND)
     print_ultrasound_status();
+#endif //INPUT_ULTRASOUND
   }
 }
 
+#if defined(INPUT_S0)
 void read_s0_inputs() {
   uint32_t current_time = millis();
   for(uint8_t i = 0; i < s0_pincount; i++) {
@@ -206,43 +311,55 @@ void read_s0_inputs() {
     last_s0_state[i] = value;
   }
 }
+#endif //INPUT_S0
 
+#if defined(INPUT_S0)
 void print_s0_status() {
   uint32_t current_time = millis();
   for(uint8_t i = 0; i < s0_pincount; i++) {
+#if defined(OUTPUT_SERIAL)
     Serial.print("s0_");
     Serial.print(s0[i].pin_name);
     Serial.print(':');
     Serial.print(last_s0_span[i]);
     Serial.print(':');
     Serial.print(impulses_since_output[i]);
-    impulses_since_output[i] = 0;
     Serial.print(':');
     Serial.print(((uint32_t)(current_time - last_s0_millis[i]))/1000);
     Serial.println(":");
+#endif //OUTPUT_SERIAL
+    impulses_since_output[i] = 0;
   }
 }
+#endif //INPUT_S0
 
+#if defined(INPUT_DIGITAL)
 void print_digital_input_status() {
   for(uint8_t i = 0; i < di_pincount; i++) {
+    uint8_t value = digitalRead(digital_input[i].pin_number);
+#if defined(OUTPUT_SERIAL)
     Serial.print("di_");
     Serial.print(digital_input[i].pin_name);
     Serial.print(':');
-    if(digitalRead(digital_input[i].pin_number) == digital_input[i].on_value) {
+    if(value == digital_input[i].on_value) {
       Serial.print("on");
     } else {
       Serial.print("off");
     }
     Serial.println(":");
+#endif //OUTPUT_SERIAL
   }
 }
+#endif //INPUT_DIGITAL
 
+#if defined(INPUT_ANALOG)
 void print_analog_input_status() {
   for(uint8_t i = 0; i < ai_pincount; i++) {
+    uint8_t value = (uint8_t)(analogRead(analog_input[i].pin_number) >> 2);
+#if defined(OUTPUT_SERIAL)
     Serial.print("ai_");
     Serial.print(analog_input[i].pin_name);
     Serial.print(':');
-    uint8_t value = (uint8_t)(analogRead(analog_input[i].pin_number) >> 2);
     Serial.print(value);
     Serial.print(':');
     if(value > analog_input[i].on_value) {
@@ -253,9 +370,12 @@ void print_analog_input_status() {
       Serial.print("mid");
     }
     Serial.println(":");
+#endif //OUTPUT_SERIAL
   }
 }
+#endif //INPUT_ANALOG
 
+#if defined(INPUT_ULTRASOUND)
 void print_ultrasound_status() {
   for(uint8_t i = 0; i < us_pincount; i++) {
     digitalWrite(us_sensor[i].trigger_pin, us_sensor[i].trigger_off);
@@ -265,6 +385,7 @@ void print_ultrasound_status() {
     digitalWrite(us_sensor[i].trigger_pin, us_sensor[i].trigger_off);
     uint32_t duration = pulseIn(us_sensor[i].echo_pin, us_sensor[i].echo_on, us_sensor[i].timeout_us);
     uint32_t distance = duration / 58;
+#if defined(OUTPUT_SERIAL)
     Serial.print("us_");
     Serial.print(us_sensor[i].sensor_name);
     Serial.print(':');
@@ -272,6 +393,8 @@ void print_ultrasound_status() {
     Serial.print(':');
     Serial.print(distance);
     Serial.println(':');
+#endif //OUTPUT_SERIAL
   }
 }
+#endif //INPUT_ULTRASOUND
 
