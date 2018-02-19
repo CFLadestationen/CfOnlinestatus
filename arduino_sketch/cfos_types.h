1b55927963e60497550144436cdd53a71bde3b38
lordprize <lordprize@users.noreply.github.com>
2018-02-19 18:18:25 +0100

    Separated sketch into multiple files
    Added a header file for config and a header file for own data types

---
+++ arduino_sketch/cfos_types.h
@@ -0,0 +1,22 @@
+#ifndef CFOS_TYPES_H
+#define CFOS_TYPES_H
+
+struct named_pin {
+  const char* pin_name;
+  const uint8_t pin_number;
+  const uint8_t on_value;
+  const uint8_t off_value;
+  const uint8_t pin_mode;
+};
+
+struct ultrasound_sensor {
+  const char* sensor_name;
+  const uint8_t trigger_pin;
+  const uint8_t echo_pin;
+  const uint32_t timeout_us;
+  const uint8_t trigger_on;
+  const uint8_t trigger_off;
+  const uint8_t echo_on;
+};
+
+#endif // CFOS_TYPES_H
